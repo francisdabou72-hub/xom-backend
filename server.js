532836996c31658b598a7100801f6f2d31e9825a
Francis Dabou <francisdabou72@gmail.com>
2025-11-29 18:07:11 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Root route
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
+// POST route for the form
+app.post('/subscribe', (req, res) => {
+  const data = req.body;
+  console.log('New subscription:', data);
+  res.send('Subscription received!');
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
